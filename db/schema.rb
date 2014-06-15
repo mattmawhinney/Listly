349ddb55ea8923b58f9f6fe356d047b252af55e2
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-06-15 12:37:10 -0400

    updated Gemfile and config/database.yml for postgres; working locally but can't push to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140614203230) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: true do |t|
     t.string   "description"
     t.integer  "list_id"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140614203230) do
     t.datetime "updated_at"
   end
 
-  add_index "items", ["list_id"], name: "index_items_on_list_id"
+  add_index "items", ["list_id"], name: "index_items_on_list_id", using: :btree
 
   create_table "lists", force: true do |t|
     t.string   "name"
